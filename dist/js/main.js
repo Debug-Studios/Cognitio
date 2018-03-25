16a9b5f03eb5dc5c29ee3ef663b4691bc25759bb
Ayush Bahuguna <ayush.bahuguna12@hotmail.com>
2018-03-26 02:11:39 +0530

    Setup gulp for website

---
+++ dist/js/main.js
@@ -0,0 +1,1 @@
+"use strict";function show(){alert("Hey! There")}
---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+const gulp = require('gulp');
+const html = require('gulp-html')
+const htmlclean = require('gulp-htmlclean');
+const imagemin = require('gulp-imagemin');
+const imageresize = require('gulp-image-resize');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+
+gulp.task('html', function(){
+    return gulp
+        .src('./src/views/*.html')
+        .pipe(html())
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('scss', function () {
+    var plugins = [autoprefixer({ browsers: ['> 5%'] }), cssnano()];
+    return gulp
+      .src('./src/scss/*.scss')
+      .pipe(sass())
+      .pipe(postcss(plugins))
+      .pipe(gulp.dest('./dist/css'));
+  });
+  
+  gulp.task('js', function () {
+    return gulp
+      .src('./src/js/*.js')
+      .pipe(
+        babel({
+          presets: ['env']
+        })
+      )
+      .pipe(uglify())
+      .pipe(gulp.dest('./dist/js'));
+  });
+  
+  gulp.task('images', function () {
+    return gulp
+      .src('./src/images/*')
+      .pipe(
+        imageresize({
+          width: 900,
+          height: 600,
+          crop: true
+        })
+      )
+      .pipe(
+        imagemin([
+          imagemin.jpegtran({
+            progressive: true
+          })
+        ])
+      )
+      .pipe(gulp.dest('./dist/img'));
+  });
+  
+  gulp.task('watch', function () {
+    gulp.watch('./src/js/*.js', ['js']);
+    gulp.watch('./src/scss/*.scss', ['scss']);
+    gulp.watch('./src/images/*', ['images']);
+    gulp.watch('./src/views/*.html', ['html']);
+  });
+  
+  gulp.task('default', ['html', 'scss', 'js', 'images']);
+  
